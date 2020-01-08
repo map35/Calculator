3b00239ac5d090414193f6cceb4bcf5d32b535f1
Mohammad Agung <diditkazama@gmail.com>
2020-01-08 20:52:42 +0700

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Mohammad Agung on 28/12/18.
+//  Copyright © 2018 Mohammad Agung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func numberPressed(_ sender: Any) {
+        
+    }
+}
+
